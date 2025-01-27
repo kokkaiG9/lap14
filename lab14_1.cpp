cfed975501f484f99b89596456a25d6646b85ec9
kokkaiG9 <gowsky.9.2548@gmail.com>
2025-01-27 17:27:38 +0700

    finishh

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,9 +2,30 @@
 using namespace std;
 
 template <typename T>
+void insertionSort(T d[],int N);
+
+template <typename T>
 void insertionSort(T d[],int N){
 
+	for (int i = 1; i < N; i++) {
+        T f = d[i];
+        int j = i - 1;
+
+        while (j >= 0 && d[j] < f) {
+            d[j+1] = d[j];
+            j = j - 1;
+        }
+        d[j + 1] = f;
+        
+        
+		cout << "Pass " << i << ":";
+		for(int x = 0 ; x < N ; x++){
+			cout << d[x] << " ";
+		}
+		cout << endl;
+    }
 }
+
 
 int main(){
 	int a[10] = {12,25,30,44,2,0,4,7,55,25};
